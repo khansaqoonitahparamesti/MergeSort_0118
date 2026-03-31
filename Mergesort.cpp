ba781664ebde1427a89f590fb8e2f10f06833ff4
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2026-03-31 11:19:09 +0700

    Handle remaining elements in merging process

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -73,6 +73,13 @@ void mergeSort(int low, int high)
         k = k + 1;     // step 4.e.iii
     }
 
+    while (i <= mid) // step 4.f
+    {
+        B[k] = arr[i]; // step 4.f.i
+        i++;           // step 4.f.ii
+        k++;           // step 4.f.iii
+    }
+
 
 
 
